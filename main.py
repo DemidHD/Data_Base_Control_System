75cad682ebe2113ffd6b9e61c69bc3522aa07b1f
danil <danillybchik94@gmail.com>
2026-06-24 10:11:26 +0300

    почему не работает?

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -42,6 +43,11 @@ async def index(request: Request):
 
 
 
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
+    )
